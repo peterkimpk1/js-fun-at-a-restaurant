2c306c22620086bca77403f571b21fac8c6ec562
Peter Kim <peterkim.pk1@gmail.com>
2024-03-20 18:46:37 -0400

    4 functions passed all tests

--- src/order.js
+++ src/order.js
@@ -1,17 +1,53 @@
 //deliveryOrder is an ordered list (array), orderNumber is an object with 5 properties
-function takeOrder(orderNumber, deliveryOrder) {
-  if (deliveryOrder.length < 3) {
-    deliveryOrder.push(orderNumber);
+
+var order1 = {};
+var order2 = {};
+var order3 = {};
+var deliveryOrders = [];
+var findOrder;
+var findIndex;
+var itemList;
+
+function takeOrder(orderNumber, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    deliveryOrders.push(orderNumber);
   }
 }
 
-function refundOrder(orderNumber, deliveryOrder) {
-  var findOrder = deliveryOrder.indexOf(Object.keys(orderNumber))
-  
+function refundOrder(returnOrderNumber, deliveryOrders) {
+  for (i = 0; i < deliveryOrders.length; i++) {
+    findOrder = deliveryOrders[i].orderNumber;
+    findIndex = i;
+    if (findOrder === returnOrderNumber) {
+      deliveryOrders.splice(findIndex,1)
+    }
+  }
 }
+
+function listItems(deliveryOrders) {
+  var listOfItems = "";
+  for (i = 0; i < deliveryOrders.length; i++) {
+    listOfItems += deliveryOrders[i].item + ", ";
+  }
+  
+  return listOfItems.substring(0, listOfItems.length-2);
+}
+
+function searchOrder(deliveryOrders,item) {
+  itemList = listItems(deliveryOrders);
+  for (i = 0; i < deliveryOrders.length; i++) {
+    if (itemList.includes(item)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+}
+
 module.exports = {
   takeOrder,
   refundOrder,
-  // listItems,
-  // searchOrder
+  listItems,
+  searchOrder
 }
