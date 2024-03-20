62f0d6b418cd5489eb0a5b64254598cc8bb14b1f
Peter Kim <peterkim.pk1@gmail.com>
2024-03-19 23:24:54 -0400

    Testing 3 functions

--- src/meal.js
+++ src/meal.js
@@ -1,11 +1,26 @@
-function nameMenuItem() {
-
+var ingredientList = [];
+var menuPrice;
+function nameMenuItem(food) {
+  return `Delicious ${food}`
 }
 
+function createMenuItem(name, price, type) {
+  var menuItem = {};
+  return menuItem;
+}
 
+function addIngredients(ingredient) {
+  if (ingredientList.includes(ingredient)) {
+  ingredientList.push(ingredient)
+  }
+}
+
+function formatPrice() {
+
+}
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
+  createMenuItem,
   // addIngredients,
   // formatPrice,
   // decreasePrice,
